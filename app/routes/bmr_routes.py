c176949e7f17bfb9bd4b85f021153fe1fe5d15fa
Ryftri <email@example.com>
2025-04-15 18:43:54 +0000

    bismillah

--- app/routes/bmr_routes.py
+++ app/routes/bmr_routes.py
@@ -20,8 +20,9 @@ def log_ip(func):
         print(f"Request received from IP: {user_ip}")
         print(f"X-RapidAPI-Host: {rapidapi_host}")
         secret = request.headers.get("X-RapidAPI-Proxy-Secret")
-        if secret != os.environ.get("RAPIDAPI_SECRET"):
-            abort(403)
+        print(f"X-RapidAPI-Proxy-Secret : {secret}")
+        # if secret != os.environ.get("RAPIDAPI_SECRET"):
+        #     abort(403)
         return func(*args, **kwargs)
     wrapper.__name__ = func.__name__
     return wrapper
